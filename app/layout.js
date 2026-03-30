990d5e14466e9645a4b5258e1ecfb3a80965c691
Hugo_Mota <hugotelemovel@gmail.com>
2026-03-30 21:01:16 +0100

    Delete app directory

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt">
-      <body style={{ margin: 0, backgroundColor: '#f1f5f9', color: '#1e293b', fontFamily: 'system-ui, sans-serif' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
